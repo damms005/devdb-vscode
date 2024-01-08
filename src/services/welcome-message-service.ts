9b1cb90e1e07f98570e8ef2e1890281dec5b3aca
Damilola Olowookere <damms005@gmail.com>
2024-01-08 22:27:00 +0000

    Fix upgrade notice

--- src/services/welcome-message-service.ts
+++ src/services/welcome-message-service.ts
@@ -15,60 +15,64 @@ export function showWelcomeMessage(context: vscode.ExtensionContext) {
 	const previousVersion = context.globalState.get<string>(
 		ExtensionConstants.globalVersionKey,
 	);
+
 	const currentVersion = vscode.extensions.getExtension(
 		ExtensionConstants.extensionId,
 	)?.packageJSON?.version;
+
 	const previousVersionArray = previousVersion
 		? previousVersion.split(".").map((s: string) => Number(s))
 		: [0, 0, 0];
+
 	const currentVersionArray = currentVersion
 		.split(".")
 		.map((s: string) => Number(s));
 
-	console.log({ previousVersion, currentVersion, previousVersionArray, currentVersionArray })
-
 	if (previousVersion === undefined || previousVersion.length === 0) {
-		message = `Thanks for using DevDb.`;
-	} else if (
-		currentVersion !== previousVersion &&
-		// patch update
-		((previousVersionArray[0] === currentVersionArray[0] &&
-			previousVersionArray[1] === currentVersionArray[1] &&
-			previousVersionArray[2] < currentVersionArray[2]) ||
-			// minor update
-			(previousVersionArray[0] === currentVersionArray[0] &&
-				previousVersionArray[1] < currentVersionArray[1]) ||
-			// major update
-			previousVersionArray[0] < currentVersionArray[0])
-	) {
-		message = `DevDb updated to ${currentVersion}.`;
+		showMessage(`Thanks for using DevDb.`)
+		return
 	}
 
-	if (message) {
-		vscode.window
-			.showInformationMessage(
-				message,
-				"⭐️ Star on Github",
-				"🐞 Report Bug",
-			)
-			.then(function (val: string | undefined) {
-				if (val === "🐞 Report Bug") {
-					vscode.env.openExternal(
-						vscode.Uri.parse(
-							"https://github.com/damms005/devdb-vscode/issues/new?assignees=&labels=bug%2Cunconfirmed%2Clow+priority&projects=&template=bug_report.yml",
-						),
-					);
-				} else if (val === "⭐️ Star on Github") {
-					vscode.env.openExternal(
-						vscode.Uri.parse(
-							"https://github.com/damms005/devdb-vscode",
-						),
-					);
-				}
-			});
-		context.globalState.update(
-			ExtensionConstants.globalVersionKey,
-			currentVersion,
-		);
+	if (currentVersion !== previousVersion) {
+
+		context.globalState.update(ExtensionConstants.globalVersionKey, currentVersion);
+
+		if (
+			// patch update
+			((previousVersionArray[0] === currentVersionArray[0] &&
+				previousVersionArray[1] === currentVersionArray[1] &&
+				previousVersionArray[2] < currentVersionArray[2]) ||
+				// minor update
+				(previousVersionArray[0] === currentVersionArray[0] &&
+					previousVersionArray[1] < currentVersionArray[1]) ||
+				// major update
+				previousVersionArray[0] < currentVersionArray[0])) {
+
+			showMessage(`DevDb updated to ${currentVersion}.`);
+		}
 	}
+}
+
+function showMessage(message: string) {
+	vscode.window
+		.showInformationMessage(
+			message,
+			"⭐️ Star on GitHub",
+			"🐞 Report Bug",
+		)
+		.then(function (val: string | undefined) {
+			if (val === "🐞 Report Bug") {
+				vscode.env.openExternal(
+					vscode.Uri.parse(
+						"https://github.com/damms005/devdb-vscode/issues/new?assignees=&labels=bug%2Cunconfirmed%2Clow+priority&projects=&template=bug_report.yml",
+					),
+				);
+			} else if (val === "⭐️ Star on Github") {
+				vscode.env.openExternal(
+					vscode.Uri.parse(
+						"https://github.com/damms005/devdb-vscode",
+					),
+				);
+			}
+		})
 }
