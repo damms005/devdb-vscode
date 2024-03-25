4972652b8ab639b4ce84d6629276bfbcf606eeb0
Damilola Olowookere <damms005@gmail.com>
2024-03-25 23:27:03 +0000

    Add version argument check to publish script

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Check if version argument is provided
+if [ -z "$1" ]; then
+  echo -e "\x1b[31mPlease specify the version (major/minor/patch)."
+  exit 1
+fi
+
 # Check if the current branch is 'main'
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ "$current_branch" != "main" ]; then
-  echo -e "\x1b[31mError:\x1b[0m The publish script can only be run from the main branch."
+  echo -e "\x1b[31mThe publish script can only be run from the main branch."
   exit 1
 fi
 
