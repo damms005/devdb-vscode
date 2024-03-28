74028e7391908c614f2787b181d121e26414bc95
Damilola Olowookere <damms005@gmail.com>
2024-03-28 10:32:28 +0000

    Update brief function to accept custom length

--- src/services/string.ts
+++ src/services/string.ts
@@ -3,11 +3,13 @@
  * adding the first 14 chars and the last 13 chars of the string together with an ellipsis in the middle.
  * e.g. /home/damms005/.SchoolServer/test-workspace/database.db3 becomes /home/damms005.../database.db3
  */
-export function brief(str: string) {
-	if (str.length <= 30) return str
+export function brief(str: string, length: number = 60) {
+	if (str.length <= length) return str
 
-	const firstPart = str.substring(0, 14)
-	const lastPart = str.substring(str.length - 13)
+	const halfLength = length / 2
+
+	const firstPart = str.substring(0, halfLength)
+	const lastPart = str.substring(str.length - halfLength)
 
 	return `${firstPart}...${lastPart}`
 }
