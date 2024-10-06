6e43b9965c296a58cf934cc488652c68d78180a3
Damilola Olowookere <damms005@gmail.com>
2024-10-06 01:47:12 +0100

    Update npm version with custom commit message if provided

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 # Check if version argument is provided
 if [ -z "$1" ]; then
-  echo -e "\x1b[31mPlease specify the version (major/minor/patch)."
+  echo -e "\x1b[31mUsage: $0 <version> [closes-<issue number> | no-closures]"
   exit 1
 fi
 
@@ -15,7 +15,7 @@ fi
 
 # cd to the dir where this script is located
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-cd $SCRIPT_DIR || exit 1
+cd "$SCRIPT_DIR" || exit 1
 echo "Current working directory:"
 pwd
 
@@ -25,20 +25,33 @@ cd ../devdb-ui || exit 1
 echo "UI build complete."
 
 echo "Building UI Shell..."
-cd $SCRIPT_DIR || exit 1
+cd "$SCRIPT_DIR" || exit 1
 cd ./ui-shell || exit 1
 ./build.sh || exit 1
 echo "UI Shell build complete."
 
-cd $SCRIPT_DIR || exit 1
+cd "$SCRIPT_DIR" || exit 1
 pwd
 
-# Bump the version using npm version
-npm version "$1" || exit 1
+# Determine commit message based on the second argument
+if [ "$2" == "no-closures" ]; then
+  commit_message=""  # Let npm version use its default message
+elif [[ "$2" =~ ^closes-[0-9]+$ ]]; then
+  issue_number="${2#closes-}"
+  commit_message="closes issue #$issue_number"
+else
+  echo -e "\x1b[31mInvalid argument for closures. Usage: $0 <version> [closes-<issue number> | no-closures]"
+  exit 1
+fi
 
-# Push the tags with git push --follow-tags. This ensure that the tags are pushed to the remote repository.
-# This works because `npm version` creates annotated tags, and `git push --follow-tags` pushes annotated tags.
-# CI/CD pipeline handles publishing to VS Code Marketplace (GitHub Actions).
+# Bump the version using npm version with custom commit message if provided
+if [ -n "$commit_message" ]; then
+  npm version "$1" -m "$commit_message" || exit 1
+else
+  npm version "$1" || exit 1
+fi
+
+# Push the tags with git push --follow-tags
 git push --follow-tags
 
 echo -e "\n\nCD pipeline will now handle publishing to VS Code Marketplace.\n\n"
