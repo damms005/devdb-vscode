f48b1565e2f95bbdb1fa5f5bf5aa18a086625890
Damilola Olowookere <damms005@gmail.com>
2024-11-29 05:45:33 +0100

    Add script to showcase new DevDb features for VS Code

---
+++ showcase.sh
@@ -0,0 +1,3 @@
+# Script to showcase new DevDb features. It opens a new VS Code instance
+# with a Profile that is preset to suit making DevDb screenshots and screen casts
+code --profile "DevDb screenshot" '/Users/damms005/Herd/nft-marketplace'
