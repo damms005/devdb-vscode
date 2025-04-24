4bfa6542786168ef3f737e89b6cf1ad20e5d3287
Damilola Olowookere <damms005@gmail.com>
2025-04-24 13:42:31 +0100

    Redo script changes

--- publish.sh
+++ publish.sh
@@ -1,62 +1,19 @@
 #!/bin/bash
 
-# Initialize variables with default values
-VERSION=""
-CLOSURE=""
+# Initialize pre-release flag
 PRE_RELEASE=false
 
-# Display usage function
-function show_usage {
-  echo -e "\x1b[33mUsage: $0 -v <version> -c [closes-<issue number> | no-closures] [-p]"
-  echo -e "\x1b[33mExample: $0 -v patch -c closes-101 -p"
-  echo -e "\x1b[33mExample: $0 -v minor -c no-closures"
-  echo -e "\x1b[33m  -v: Version (patch, minor, major, etc.)"
-  echo -e "\x1b[33m  -c: Closure information (closes-<issue number> or no-closures)"
-  echo -e "\x1b[33m  -p: Enable pre-release mode\x1b[0m"
-}
-
 # Parse arguments
-while getopts "v:c:p" opt; do
-  case ${opt} in
-    v )
-      VERSION=$OPTARG
-      ;;
-    c )
-      CLOSURE=$OPTARG
-      ;;
-    p )
-      PRE_RELEASE=true
-      ;;
-    \? )
-      echo -e "\x1b[31mInvalid option: $OPTARG" 1>&2
-      show_usage
-      exit 1
-      ;;
-    : )
-      echo -e "\x1b[31mInvalid option: $OPTARG requires an argument" 1>&2
-      show_usage
-      exit 1
-      ;;
-  esac
+for arg in "$@"; do
+  if [ "$arg" = "--pre-release" ]; then
+    PRE_RELEASE=true
+    break
+  fi
 done
 
-# Validate required arguments
-if [ -z "$VERSION" ]; then
-  echo -e "\x1b[31mError: Version is required (-v)"
-  show_usage
-  exit 1
-fi
-
-if [ -z "$CLOSURE" ]; then
-  echo -e "\x1b[31mError: Closure information is required (-c)"
-  show_usage
-  exit 1
-fi
-
-# Validate closure format
-if [ "$CLOSURE" != "no-closures" ] && [[ ! "$CLOSURE" =~ ^closes-[0-9]+$ ]]; then
-  echo -e "\x1b[31mError: Invalid closure format. Must be 'no-closures' or 'closes-<issue number>'"
-  show_usage
+# Check if version argument is provided
+if [ -z "$1" ]; then
+  echo -e "\x1b[31mUsage: $0 <version> [closes-<issue number> | no-closures] [--pre-release]"
   exit 1
 fi
 
@@ -95,22 +52,25 @@ echo "UI Shell build complete."
 cd "$SCRIPT_DIR" || exit 1
 pwd
 
-# Determine commit message based on the closure argument
-if [ "$CLOSURE" == "no-closures" ]; then
+# Determine commit message based on the second argument
+if [ "$2" == "no-closures" ]; then
   commit_body=""  # No closure message
-elif [[ "$CLOSURE" =~ ^closes-[0-9]+$ ]]; then
-  issue_number="${CLOSURE#closes-}"
+elif [[ "$2" =~ ^closes-[0-9]+$ ]]; then
+  issue_number="${2#closes-}"
   commit_body="Closes #$issue_number"  # GitHub auto-close format
+else
+  echo -e "\x1b[31mInvalid argument for closures. Usage: $0 <version> [closes-<issue number> | no-closures]"
+  exit 1
 fi
 
 # Bump the version using npm version
 if [ -n "$commit_body" ]; then
   # Construct the commit message with printf to handle newlines properly
   commit_message=$(printf "%%s\n\n%s" "$commit_body")
-  npm version "$VERSION" -m "$commit_message" || exit 1
+  npm version "$1" -m "$commit_message" || exit 1
 else
   # Just use npm version without custom commit message body
-  npm version "$VERSION" || exit 1
+  npm version "$1" || exit 1
 fi
 
 # If this is a pre-release, check if we need to bump the minor version
